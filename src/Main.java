56ddf3e97a903a11d5d4a6385ce45e03e4f131af
zanydruid <zanydruid528@gmail.com>
2017-01-14 22:10:59 -0500

    Fix main

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,6 @@ import java.util.Arrays;
 
 public class Main {
 
-    public static final String PARTNER_ID_PATH_SEPARATOR = ">";
-
-
     public static void main (String[] args) throws InterruptedException, IOException {
         int i = 0;
         ++i;
