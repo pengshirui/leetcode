4d421c504c5c201c9c4381a1b67f774b0c744489
Shirui Peng <shirui.ryan.peng@gmail.com>
2021-11-05 22:57:32 -0400

    NumBusesToDestination

---
+++ csharp/LeetCode/BFS/NumBusesToDestination.cs
@@ -0,0 +1,65 @@
+﻿namespace LeetCode.BFS.NumBusesToDestination
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Solution
+    {
+        Queue<int> q = new Queue<int>();
+        Dictionary<int, List<int>> kv = new Dictionary<int, List<int>>();
+        HashSet<int> visitedStop = new HashSet<int>();
+        HashSet<int> visitedBus = new HashSet<int>();
+        public int NumBusesToDestination(int[][] routes, int source, int target)
+        {
+            if (source == target) return 0;
+            for (var i = 0; i < routes.Length; i++)
+            {
+                for (var j = 0; j < routes[i].Length; j++)
+                {
+                    // i: bus id;
+                    // routes[i][j]: stop id;
+                    kv.TryAdd(routes[i][j], new List<int>());
+                    kv[routes[i][j]].Add(i);
+                }
+            }
+            if (!kv.ContainsKey(target)) return -1;
+            var res = 0;
+            q.Enqueue(source);
+            visitedStop.Add(source);
+
+            while (q.Any())
+            {
+                var c = q.Count;
+                while (c-- > 0)
+                {
+                    var stop = q.Dequeue();
+                    if (stop == target)
+                    {
+                        return res;
+                    }
+                    Add(routes, stop);
+                }
+                res++;
+            }
+
+            return -1;
+        }
+
+        void Add(int[][] routes, int stop)
+        {
+            foreach (var bus in kv[stop])
+            {
+                if (visitedBus.Contains(bus)) continue;
+                foreach (var next in routes[bus])
+                {
+                    if (visitedStop.Contains(next)) continue;
+                    q.Enqueue(next);
+                    visitedStop.Add(next);
+                }
+
+                visitedBus.Add(bus);
+            }
+        }
+    }
+
+}
---
+++ csharp/UnitTest/BFS/NumBusesToDestinationTest.cs
@@ -0,0 +1,34 @@
+﻿namespace UnitTest.BFS
+{
+    using FluentAssertions;
+    using LeetCode.BFS.NumBusesToDestination;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class NumBusesToDestinationTest
+    {
+        readonly Solution Solution = new Solution();
+
+        [TestMethod]
+        public void Test1()
+        {
+            var g1 = new int[][]
+            {
+                new int[] { 1, 2, 7 },
+                new int[] { 3, 6, 7 },
+            };
+            Solution.NumBusesToDestination(g1, 1, 6).Should().Be(2);
+        }
+
+        [TestMethod]
+        public void Test2()
+        {
+            var g1 = new int[][]
+            {
+                new int[] { 1, 2, 7 },
+                new int[] { 3, 6, 7 },
+            };
+            Solution.NumBusesToDestination(g1, 1, 9).Should().Be(-1);
+        }
+    }
+}
