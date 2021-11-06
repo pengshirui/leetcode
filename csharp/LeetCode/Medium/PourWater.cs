3429b9c3dd3519312c140ad5fc63eeb5f3415be1
Shirui Peng <shirui.ryan.peng@gmail.com>
2021-11-05 21:13:57 -0400

    PourWater

--- csharp/LeetCode/Medium/PourWater.cs
+++ csharp/LeetCode/Medium/PourWater.cs
@@ -1,14 +1,29 @@
 ﻿namespace LeetCode.Medium.PourWater
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Text;
-
     public class Solution
     {
         public int[] PourWater(int[] heights, int volume, int k)
         {
-            return null;
+            while (volume-- > 0)
+            {
+                var c = k;
+                while (c > 0 && heights[c - 1] <= heights[c])
+                {
+                    c--;
+                }
+
+                while (c < heights.Length - 1 && heights[c ] >= heights[c + 1])
+                {
+                    c++;
+                }
+                
+                while (c > k && heights[c - 1] == heights[c])
+                {
+                    c--;
+                }
+                heights[c]++;
+            }
+            return heights;
         }
     }
 }
