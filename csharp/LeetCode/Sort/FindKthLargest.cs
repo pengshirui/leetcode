29563de4cc1de8bfd12f9ac28edc01ae1d3e2d28
Shirui Peng <shirui.ryan.peng@gmail.com>
2021-11-07 00:05:43 -0400

    FindKthLargest

---
+++ csharp/LeetCode/Sort/FindKthLargest.cs
@@ -0,0 +1,50 @@
+﻿namespace LeetCode.Sort.FindKthLargest
+{
+    public class Solution
+    {
+        int[] Nums;
+        public int FindKthLargest(int[] nums, int k)
+        {
+            Nums = nums;
+            return Find(0, nums.Length - 1, nums.Length - k);
+        }
+
+        int Find(int l, int r, int k)
+        {
+            if (l == r) return Nums[k];
+
+            var i = l - 1;
+            var j = r + 1;
+            var m = Nums[l];
+            while (i < j)
+            {
+                do
+                {
+                    i++;
+                }
+                while (Nums[i] < m);
+
+                do
+                {
+                    j--;
+                }
+                while (Nums[j] > m);
+
+                if (i < j)
+                {
+                    var t = Nums[i];
+                    Nums[i] = Nums[j];
+                    Nums[j] = t;
+                }
+            }
+            if (k <= j)
+            {
+                return Find(l, j, k);
+            }
+            else
+            {
+                return Find(j + 1, r, k);
+            }
+        }
+    }
+}
---
+++ csharp/UnitTest/Sort/FindKthLargestTest.cs
@@ -0,0 +1,44 @@
+﻿namespace UnitTest.Sort
+{
+    using FluentAssertions;
+    using LeetCode.Sort.FindKthLargest;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class FindKthLargestTest
+    {
+        private readonly Solution Solution = new Solution();
+
+        [TestMethod]
+        public void Test1()
+        {
+            int[] n = { 3, 2, 1, 5, 6, 4 };
+            var k = 2;
+            Solution.FindKthLargest(n, k).Should().Be(5);
+        }
+
+        [TestMethod]
+        public void Test2()
+        {
+            int[] n = { 3, 2, 1, 5, 6, 4 };
+            var k = 6;
+            Solution.FindKthLargest(n, k).Should().Be(1);
+        }
+
+        [TestMethod]
+        public void Test3()
+        {
+            int[] n = { 3, 2, 1, 5, 6, 4 };
+            var k = 1;
+            Solution.FindKthLargest(n, k).Should().Be(6);
+        }
+
+        [TestMethod]
+        public void Test4()
+        {
+            int[] n = { 1, 1, 1};
+            var k = 1;
+            Solution.FindKthLargest(n, k).Should().Be(1);
+        }
+    }
+}
