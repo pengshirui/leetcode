3612c06a1126924641c0ba36484fc15770fde346
Shirui Peng <shirui.ryan.peng@gmail.com>
2021-11-06 21:25:44 -0400

    TotalNQueens

---
+++ csharp/LeetCode/DFS/TotalNQueens.cs
@@ -0,0 +1,45 @@
+﻿namespace LeetCode.DFS.TotalNQueens
+{
+    public class Solution
+    {
+        int res;
+        int N;
+        public int TotalNQueens(int n)
+        {
+            N = n;
+            DFS(new int[n], 0);
+            return res;
+        }
+
+        void DFS(int[] board, int i)
+        {
+            if (i == N)
+            {
+                res++;
+                return;
+            }
+
+            for (var j = 0; j < N; j++)
+            {
+                if (Check(board, i, j))
+                {
+                    board[i] = j;
+                    DFS(board, i + 1);
+                }
+            }
+        }
+
+        bool Check(int[] b, int x, int y)
+        {
+            for (var i = 0; i < x; i++)
+            {
+                var j = b[i];
+                if (j == y || i + j == x + y || i - j == x - y)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
