3e6b59fdf673d7fcba323dd4533b92d4809ee7b9
Shirui Peng <shirui.ryan.peng@gmail.com>
2021-11-07 21:58:05 -0500

    NumMatrix

---
+++ csharp/LeetCode/Maths/CubeRoot.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCode.Maths.CubeRoot
+{
+    public class Solution
+    {
+        public double CubeRoot(int n)
+        {
+            double l = -10000;
+            double r = 10000;
+            while (r - l > 1e-8)
+            {
+                var m = (r + l) / 2;
+                if (m * m * m <= n)
+                {
+                    l = m;
+                }
+                else
+                {
+                    r = m;
+                }
+            }
+            return l;
+        }
+    }
+}
--- csharp/LeetCode/Medium/NumMatrix.cs
+++ csharp/LeetCode/Medium/NumMatrix.cs
@@ -2,35 +2,30 @@
 {
     public class NumMatrix
     {
-        private readonly int[][] Memo;
+        int[,] sum;
+        int m, n;
+
         public NumMatrix(int[][] matrix)
         {
-            Memo = new int[matrix.Length][];
-
-            for (var i = 0; i < matrix.Length; i++)
+            m = matrix.Length;
+            n = matrix[0].Length;
+            sum = new int[m + 1, n + 1];
+            for (var i = 1; i <= m; i++)
             {
-                for (var j = 0; j < matrix[i].Length; j++)
+                for (var j = 1; j <= n; j++)
                 {
-                    if (j == 0)
-                    {
-                        Memo[i] = new int[matrix[i].Length];
-                    }
-                    var a = i - 1 >= 0 ? Memo[i - 1][j] : 0;
-                    var b = j - 1 >= 0 ? Memo[i][j - 1] : 0;
-                    var c = i - 1 >= 0 && j - 1 >= 0 ? Memo[i - 1][j - 1] : 0;
-                    Memo[i][j] = a + b - c + matrix[i][j];
+                    sum[i, j] = matrix[i - 1][j - 1] + sum[i - 1, j] + sum[i, j - 1] - sum[i - 1, j - 1];
                 }
             }
         }
 
         public int SumRegion(int row1, int col1, int row2, int col2)
         {
-            var a = row1 - 1 >= 0 && col1 - 1 >= 0 ? Memo[row1 - 1][col1 - 1] : 0;
-            var b = col1 - 1 >= 0 ? Memo[row2][col1 - 1] : 0;
-            var c = row1 - 1 >= 0 ? Memo[row1 - 1][col2] : 0;
-            var d = Memo[row2][col2];
-
-            return d - c - b + a;
+            var a = sum[row2 + 1, col2 + 1];
+            var b = sum[row1, col2 + 1];
+            var c = sum[row2 + 1, col1];
+            var d = sum[row1, col1];
+            return a - b - c + d;
         }
     }
 }
