37d8c9df2431da23cfde08a8a6d6fc113bd35c0f
Shirui Peng <shirui.ryan.peng@gmail.com>
2021-11-07 20:39:27 -0500

    AlienOrder

--- csharp/LeetCode/Hard/AlienOrder.cs
+++ csharp/LeetCode/Hard/AlienOrder.cs
@@ -1,84 +1,79 @@
 ﻿namespace LeetCode.Hard.AlienOrder
 {
+    using System;
     using System.Collections.Generic;
     using System.Linq;
     using System.Text;
 
     public class Solution
     {
+        StringBuilder Res = new StringBuilder();
+        List<int>[] Next = new List<int>[26];
+        int[] Indegree = new int[26];
+        Queue<int> q = new Queue<int>();
         public string AlienOrder(params string[] words)
         {
-            var nodes = new Node[26];
-            foreach (var word in words)
+            Array.Fill(Indegree, -1);
+            foreach (var w in words)
             {
-                foreach (var c in word)
+                foreach (var c in w)
                 {
-                    if (nodes[c - 'a'] == null) nodes[c - 'a'] = new Node(c);
+                    if (Next[c - 'a'] == null) Next[c - 'a'] = new List<int>();
+                    Indegree[c - 'a'] = 0;
                 }
             }
+
             for (var i = 1; i < words.Length; i++)
             {
-                // Check that word2 is not a prefix of word1.
-                if (words[i - 1].Length > words[i].Length && words[i - 1].StartsWith(words[i]))
+                var w0 = words[i - 1];
+                var w1 = words[i];
+                var len = Math.Min(w0.Length, w1.Length);
+                var j = 0;
+                while (j < len && w0[j] == w1[j])
                 {
-                    return string.Empty;
+                    j++;
                 }
-                for (var j = 0; j < words[i].Length && j < words[i - 1].Length; j++)
-                {
-                    if (words[i - 1][j] != words[i][j])
-                    {
-                        var a = words[i - 1][j];
-                        var b = words[i][j];
-
-                        var first = nodes[a - 'a'];
-                        var second = nodes[b - 'a'];
-                        first.Next.Add(second);
-                        second.Indegree++;
-                        break;
-                    }
-                }
-
+                if (j == len && w0.Length > w1.Length) return string.Empty;
+                if (j >= len) continue;
+                var u = w0[j];
+                var v = w1[j];
+                Next[u - 'a'].Add(v - 'a');
+                Indegree[v - 'a']++;
             }
 
-            // Add 0 indegree node
-            var q = new Queue<Node>();
-            var totalNodes = 0;
-            foreach (var n in nodes)
+            return TopSort();
+        }
+
+        string TopSort()
+        {
+            var cnt1 = 0;
+            for (var i = 0; i < 26; i++)
             {
-
-                if (n != null)
+                if (Indegree[i] == 0)
                 {
-                    totalNodes++;
-                    if (n.Indegree == 0) q.Enqueue(n);
+                    q.Enqueue(i);
+                }
+                if (Indegree[i] != -1)
+                {
+                    cnt1++;
                 }
             }
-
-            var result = new StringBuilder();
+            var cnt2 = 0;
             while (q.Any())
             {
-                var node = q.Dequeue();
-                result.Append(node.Val);
-                foreach (var nextNode in node.Next)
+                var c = q.Dequeue();
+                Res.Append((char)(c + 'a'));
+                cnt2++;
+                foreach (var n in Next[c])
                 {
-                    nextNode.Indegree--;
-                    if (nextNode.Indegree == 0)
+                    Indegree[n]--;
+                    if (Indegree[n] == 0)
                     {
-                        q.Enqueue(nextNode);
+                        q.Enqueue(n);
                     }
                 }
             }
-            return result.Length == totalNodes ? result.ToString() : string.Empty;
-        }
-    }
-
-    class Node
-    {
-        public char Val;
-        public IList<Node> Next = new List<Node>();
-        public int Indegree = 0;
-        public Node(char c)
-        {
-            Val = c;
+            return cnt1 == cnt2 ? Res.ToString() : string.Empty;
         }
     }
 }
