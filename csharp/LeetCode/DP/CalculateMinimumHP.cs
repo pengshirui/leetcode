586e98558039892fe84a7feec922d2d4bd0a58e0
Shirui Peng <shirui.ryan.peng@gmail.com>
2021-11-11 23:26:37 -0500

    CalculateMinimumHP

---
+++ csharp/LeetCode/DP/CalculateMinimumHP.cs
@@ -0,0 +1,33 @@
+﻿namespace LeetCode.DP.CalculateMinimumHP
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class Solution
+    {
+        int INF = int.MaxValue / 2;
+        int[,] f;
+        public int CalculateMinimumHP(int[][] dungeon)
+        {
+            var m = dungeon.Length;
+            var n = dungeon[0].Length;
+            f = new int[m + 1, n + 1];
+            for (var i = 0; i <= m; i++)
+                for (var j = 0; j <= n; j++) f[i, j] = INF;
+
+            f[m, n - 1] = 1;
+            f[m - 1, n] = 1;
+
+            for (var i = m - 1; i >= 0; i--)
+            {
+                for (var j = n - 1; j >= 0; j--)
+                {
+                    var hp = Math.Min(f[i, j + 1], f[i + 1, j]) - dungeon[i][j];
+                    f[i, j] = hp <= 0 ? 1 : hp;
+                }
+            }
+            return f[0, 0];
+        }
+    }
+}
---
+++ csharp/UnitTest/DP/CalculateMinimumHPTest.cs
@@ -0,0 +1,24 @@
+﻿namespace UnitTest.DP
+{
+    using FluentAssertions;
+    using LeetCode.DP.CalculateMinimumHP;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class CalculateMinimumHPTest
+    {
+        readonly Solution Solution = new Solution();
+
+        [TestMethod]
+        public void Test1()
+        {
+            int[][] d =
+            {
+                new int[] { -2, -3, 3 },
+                new int[] { -5, -10, 1 },
+                new int[] { 10, 30, -5 },
+            };
+            Solution.CalculateMinimumHP(d).Should().Be(7);
+        }
+    }
+}
