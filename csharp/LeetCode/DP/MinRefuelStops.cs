86daad90e35a14c679dbaf530165a71d9a7c0437
Shirui Peng <shirui.ryan.peng@gmail.com>
2021-11-14 16:32:33 -0500

    MinRefuelStops

---
+++ csharp/LeetCode/DP/MinRefuelStops.cs
@@ -0,0 +1,34 @@
+﻿namespace LeetCode.DP.MinRefuelStops
+{
+    using System;
+
+    public class Solution
+    {
+        int n;
+        int[] f;
+
+        public int MinRefuelStops(int target, int startFuel, int[][] stations)
+        {
+            n = stations.Length;
+            f = new int[n + 1];
+            Array.Fill(f, startFuel);
+
+            for (var i = 1; i <= n; i++)
+            {
+                for (var j = i; j >= 1; j--)
+                {
+                    if (f[j - 1] >= stations[i - 1][0])
+                    {
+                        f[j] = Math.Max(f[j], f[j - 1] + stations[i - 1][1]);
+                    }
+                }
+            }
+
+            for (var i = 0; i <= n; i++)
+            {
+                if (f[i] >= target) return i;
+            }
+            return -1;
+        }
+    }
+}
---
+++ csharp/UnitTest/DP/MinRefuelStopsTest.cs
@@ -0,0 +1,26 @@
+﻿namespace UnitTest.DP
+{
+    using FluentAssertions;
+    using LeetCode.DP.MinRefuelStops;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class MinRefuelStopsTest
+    {
+        readonly Solution Solution = new Solution();
+
+        [TestMethod]
+        public void Test1()
+        {
+
+            int[][] s = 
+            { 
+                new int[] { 10, 60 },
+                new int[] { 20, 30 },
+                new int[] { 30, 30 },
+                new int[] { 60, 40 },
+             };
+            Solution.MinRefuelStops(100, 10, s).Should().Be(2);
+        }
+    }
+}
