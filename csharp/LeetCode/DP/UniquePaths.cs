ca355417eb95541a513250f70e9d67533b6ecb4b
Shirui Peng <shirui.ryan.peng@gmail.com>
2021-11-16 21:06:48 -0500

    Update

---
+++ csharp/LeetCode/DP/UniquePaths.cs
@@ -0,0 +1,20 @@
+﻿namespace LeetCode.DP.UniquePaths
+{
+    public class Solution
+    {
+        public int UniquePaths(int m, int n)
+        {
+            var f = new int[m + 1, n + 1];
+            f[1, 1] = 1;
+            for (var i = 1; i <= m; i++)
+            {
+                for (var j = 1; j <= n; j++)
+                {
+                    if (i == j && i == 1) continue;
+                    f[i, j] = f[i - 1, j] + f[i, j - 1];
+                }
+            }
+            return f[m, n];
+        }
+    }
+}
---
+++ csharp/UnitTest/DP/UniquePathsTest.cs
@@ -0,0 +1,18 @@
+﻿namespace UnitTest.DP
+{
+    using FluentAssertions;
+    using LeetCode.DP.UniquePaths;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class UniquePathsTest
+    {
+        readonly Solution Solution = new Solution();
+
+        [TestMethod]
+        public void Test1()
+        {
+            Solution.UniquePaths(3, 2).Should().Be(3);
+        }
+    }
+}
