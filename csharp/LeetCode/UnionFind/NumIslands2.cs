d2f16a0f9f9f75f9c73cd2d615b173a90c66b38d
Shirui Peng <shirui.ryan.peng@gmail.com>
2021-11-16 22:07:14 -0500

    NumIslands2

---
+++ csharp/LeetCode/UnionFind/NumIslands2.cs
@@ -0,0 +1,60 @@
+﻿namespace LeetCode.UnionFind.NumIslands2
+{
+    using System.Collections.Generic;
+
+    public class Solution
+    {
+        int[] p;
+        int[,] Dxy = { { 1, 0 }, { -1, 0 }, { 0, 1 }, { 0, -1 } };
+        HashSet<int> hs = new HashSet<int>();
+        public IList<int> NumIslands2(int m, int n, int[][] positions)
+        {
+            p = new int[m * n];
+            for (var i = 0; i < p.Length; i++) p[i] = i;
+
+            var res = new int[positions.Length];
+
+            var cnt = 0;
+            for (var i = 0; i < positions.Length; i++)
+            {
+                var x = positions[i][0];
+                var y = positions[i][1];
+                var k = x * n + y;
+                if (hs.Contains(k))
+                {
+                    res[i] = cnt;
+                    continue;
+                };
+                hs.Add(k);
+                var change = 1;
+                for (var d = 0; d < 4; d++)
+                {
+                    var x0 = x + Dxy[d, 0];
+                    var y0 = y + Dxy[d, 1];
+                    var k0 = x0 * n + y0;
+                    if (x0 < 0 || x0 >= m || y0 < 0 || y0 >= n) continue;
+                    if (!hs.Contains(k0)) continue;
+                    var rk = Find(k);
+                    var rk0 = Find(k0);
+                    if (rk != rk0)
+                    {
+                        p[rk] = rk0;
+                        change--;
+                    }
+                }
+                cnt += change;
+                res[i] = cnt;
+            }
+            return res;
+        }
+
+        int Find(int x)
+        {
+            while (x != p[x])
+            {
+                x = p[x];
+            }
+            return x;
+        }
+    }
+}
---
+++ csharp/UnitTest/UnionFind/NumIslands2Test.cs
@@ -0,0 +1,55 @@
+﻿namespace UnitTest.UnionFind
+{
+    using FluentAssertions;
+    using LeetCode.UnionFind.NumIslands2;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class NumIslands2Test
+    {
+        readonly Solution Solution = new Solution();
+
+        [TestMethod]
+        public void Test1()
+        {
+            int[][] m = 
+            {
+                new int[] {0, 1},
+                new int[] {1, 2},
+                new int[] {2, 1},
+                new int[] {1, 0},
+                new int[] {0, 2},
+                new int[] {0, 0},
+                new int[] {1, 1},
+            };
+            Solution.NumIslands2(3, 3, m).Should().Equal(1, 2, 3, 4, 3, 2, 1);
+        }
+
+        [TestMethod]
+        public void Test2()
+        {
+            int[][] m =
+            {
+                new int[] {0, 0},
+                new int[] {0, 1},
+                new int[] {1, 2},
+                new int[] {1, 2},
+            };
+            Solution.NumIslands2(3, 3, m).Should().Equal(1, 1, 2, 2);
+        }
+
+        [TestMethod]
+        public void Test3()
+        {
+            int[][] m =
+            {
+                new int[] {0, 0},
+                new int[] {7, 1},
+                new int[] {6, 1},
+                new int[] {3, 3},
+                new int[] {4, 1},
+            };
+            Solution.NumIslands2(8, 4, m).Should().Equal(1, 2, 2, 3, 4);
+        }
+    }
+}
