eb2c56937002c541fab334e5ffe881633fb7b2a1
Shirui Peng <shirui.ryan.peng@gmail.com>
2021-11-20 18:02:38 -0500

    MinDifficulty

--- csharp/LeetCode/DP/MinDifficulty.cs
+++ csharp/LeetCode/DP/MinDifficulty.cs
@@ -4,34 +4,30 @@
 
     public class Solution
     {
-        int m, n;
+        int INF = int.MaxValue / 2;
         public int MinDifficulty(int[] jobDifficulty, int d)
         {
-            if (d > jobDifficulty.Length) return -1;
-            m = jobDifficulty.Length;
-            n = d;
-            var f = new int[m + 1, n + 1];
-            var mw = new int[m + 1];
+            var n = jobDifficulty.Length;
+            if (n < d) return -1;
+            var f = new int[n + 1, d + 1];
 
+            for (var i = 1; i <= n; i++)
+                for (var j = 0; j < i && j <= d; j++) f[i, j] = INF;
 
-            for (var i = 1; i <= m; i++)
+            for (var i = 1; i <= n; i++)
             {
-                var job = jobDifficulty[i - 1];
-                for (var j = 1; j <= i; j++)
+                for (var j = 1; j <= i && j <= d; j++)
                 {
-                    mw[i] = job;
-                    for (var k = i - 1; k > j; k--)
-                    {
-                        mw[k] = Math.Max(mw[k + 1], jobDifficulty[k - 1]);
-                    }
                     f[i, j] = int.MaxValue;
-                    for (var k = i; k >= j; k--)
+                    var job = jobDifficulty[i - 1];
+                    for (var k = 1; i - k >= j - 1; k++)
                     {
-                        f[i, j] = Math.Min(f[i, j], f[k - 1, j - 1] + mw[k]);
+                        job = Math.Max(job, jobDifficulty[i - k]);
+                        f[i, j] = Math.Min(f[i, j], f[i - k, j - 1] + job);
                     }
                 }
             }
-            return f[m, n];
+            return f[n, d];
         }
     }
 }
