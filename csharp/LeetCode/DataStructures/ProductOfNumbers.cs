b03675304362a02edf0d1557374c2f457f85d7c1
Shirui Peng <shirui.ryan.peng@gmail.com>
2021-11-21 00:11:45 -0500

    ProductOfNumbers

---
+++ csharp/LeetCode/DataStructures/ProductOfNumbers.cs
@@ -0,0 +1,32 @@
+﻿namespace LeetCode.DataStructures
+{
+    public class ProductOfNumbers
+    {
+        const int N = (int)4e4 + 10;
+        int cnt = 1;
+        int[] arr = new int[N];
+
+        public ProductOfNumbers()
+        {
+            arr[0] = 1;
+        }
+
+        public void Add(int num)
+        {
+            if (num == 0)
+            {
+                cnt = 1;
+            }
+            else
+            {
+                arr[cnt] = arr[cnt - 1] * num;
+                cnt++;
+            }
+        }
+
+        public int GetProduct(int k)
+        {
+            return cnt - k - 1 < 0 ? 0 : arr[cnt - 1] / arr[cnt - k - 1];
+        }
+    }
+}
---
+++ csharp/UnitTest/DataStructures/ProductOfNumbersTest.cs
@@ -0,0 +1,26 @@
+﻿namespace UnitTest.DataStructures
+{
+    using FluentAssertions;
+    using LeetCode.DataStructures;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class ProductOfNumbersTest
+    {
+        [TestMethod]
+        public void Test1()
+        {
+            var s = new ProductOfNumbers();
+            s.Add(3);
+            s.Add(0);
+            s.Add(2);
+            s.Add(5);
+            s.Add(4);
+            s.GetProduct(2).Should().Be(20);
+            s.GetProduct(3).Should().Be(40);
+            s.GetProduct(4).Should().Be(0);
+            s.Add(8);
+            s.GetProduct(2).Should().Be(32);
+        }
+    }
+}
