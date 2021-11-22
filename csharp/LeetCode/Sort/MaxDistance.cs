0fac7d41b07d6b5aa880e1476a4376181b4ef8d4
Shirui Peng <shirui.ryan.peng@gmail.com>
2021-11-21 21:54:38 -0500

    MaxDistance

---
+++ csharp/LeetCode/Sort/MaxDistance.cs
@@ -0,0 +1,41 @@
+﻿namespace LeetCode.Sort.MaxDistance
+{
+    using System;
+
+    public class Solution
+    {
+        public int MaxDistance(int[] position, int m)
+        {
+            Array.Sort(position);
+
+            var l = 1;
+            var r = position[position.Length - 1] - position[0];
+            while (l < r)
+            {
+                var mi = l + r + 1 >> 1;
+                if (Check(position, mi, m - 1))
+                {
+                    l = mi;
+                }
+                else
+                {
+                    r = mi - 1;
+                }
+            }
+            return l;
+        }
+
+        bool Check(int[] nums, int d, int m)
+        {
+            var i = 0;
+            while (i < nums.Length && m-- > 0)
+            {
+                int j = i;
+                while (j < nums.Length && nums[j] - nums[i] < d) j++;
+                if (j >= nums.Length) return false;
+                i = j;
+            }
+            return true;
+        }
+    }
+}
---
+++ csharp/UnitTest/Sort/MaxDistanceTest.cs
@@ -0,0 +1,28 @@
+﻿namespace UnitTest.Sort
+{
+    using FluentAssertions;
+    using LeetCode.Sort.MaxDistance;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class MaxDistanceTest
+    {
+        private readonly Solution Solution = new Solution();
+
+        [TestMethod]
+        public void Test1()
+        {
+            int[] n = { 1, 2, 3, 4, 7 };
+            var k = 3;
+            Solution.MaxDistance(n, k).Should().Be(3);
+        }
+
+        [TestMethod]
+        public void Test2()
+        {
+            int[] n = { 5, 4, 3, 2, 1, 1000000000 };
+            var k = 2;
+            Solution.MaxDistance(n, k).Should().Be(999999999);
+        }
+    }
+}
