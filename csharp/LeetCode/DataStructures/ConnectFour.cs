6cb2250fda592781a9ac9981e58a2ed30536a538
Shirui Peng <shirui.ryan.peng@gmail.com>
2021-11-29 23:15:06 -0500

    ConnectFour

---
+++ csharp/LeetCode/DataStructures/ConnectFour.cs
@@ -0,0 +1,55 @@
+﻿namespace LeetCode.DataStructures
+{
+    public class ConnectFour
+    {
+        const int m = 7;
+        const int n = 6;
+        int[, ] b = new int[m, n];
+        int cnt = 0;
+        int p = 1;
+        int[] ds = { 0, -1, 1 };
+        int res;
+        int[] rows = new int[m];
+        public ConnectFour()
+        {
+
+        }
+
+        public int Place(int x)
+        {
+            cnt++;
+            var y = rows[x];
+            if (x < 0 || x >= m || y >= n) return - 1;
+            b[x, y] = p;
+            rows[x]++;
+            if (Win(x, y)) return p;
+            if (cnt == m * n) return -2;
+            p = 3 - p;
+            return 0;
+        }
+
+        bool Win(int x, int y)
+        {
+            for (var i = 0; i < 3; i++)
+            {
+                for (var j = 0; j < 3; j++)
+                {
+                    if (i == j && i == 0) continue;
+                    res = 0;
+                    DFS(x, y, ds[i], ds[j]);
+                    if (res == 4) return true;
+                    res = 0;
+                }
+            }
+            return false;
+        }
+
+        void DFS(int x, int y, int dx, int dy)
+        {
+            if (x < 0 || x >= m || y < 0 || y >= n || b[x, y] != p) return;
+            res++;
+            if (res == 4) return;
+            DFS(x + dx, y + dy, dx, dy);
+        }
+    }
+}
---
+++ csharp/UnitTest/DataStructures/ConnectFourTest.cs
@@ -0,0 +1,53 @@
+﻿namespace UnitTest.DataStructures
+{
+    using FluentAssertions;
+    using LeetCode.DataStructures;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class ConnectFourTest
+    {
+        [TestMethod]
+        public void Test1()
+        {
+            var cf = new ConnectFour();
+            cf.Place(1).Should().Be(0); //r
+            cf.Place(1).Should().Be(0); //g
+            cf.Place(1).Should().Be(0); //r
+            cf.Place(1).Should().Be(0); //g
+            cf.Place(1).Should().Be(0); //r
+            cf.Place(1).Should().Be(0); //g
+            cf.Place(1).Should().Be(-1); //r
+            cf.Place(0).Should().Be(0);  //r
+            cf.Place(0).Should().Be(0);  //g
+            cf.Place(0).Should().Be(0);  //r
+            cf.Place(0).Should().Be(0);  //g
+            cf.Place(0).Should().Be(0);  //r
+            cf.Place(0).Should().Be(0);  //g
+            cf.Place(0).Should().Be(-1);  //r
+            cf.Place(2).Should().Be(0);  //r
+            cf.Place(2).Should().Be(0);  //g
+            cf.Place(2).Should().Be(0);  //r
+            cf.Place(2).Should().Be(0);  //g
+            cf.Place(2).Should().Be(0);  //r
+            cf.Place(2).Should().Be(0);  //g
+            cf.Place(3).Should().Be(1);  //g
+        }
+
+        [TestMethod]
+        public void Test2()
+        {
+            var cf = new ConnectFour();
+            cf.Place(2).Should().Be(0); //r
+            cf.Place(2).Should().Be(0); //g
+            cf.Place(3).Should().Be(0); //r
+            cf.Place(1).Should().Be(0); //g
+            cf.Place(3).Should().Be(0); //r
+            cf.Place(3).Should().Be(0); //g
+            cf.Place(4).Should().Be(0); //r
+            cf.Place(4).Should().Be(0); //g
+            cf.Place(4).Should().Be(0); //r
+            cf.Place(4).Should().Be(2); //g
+        }
+    }
+}
