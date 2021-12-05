8debc5d3407f36b436d8eec587587c415f29fede
Shirui Peng <shirui.ryan.peng@gmail.com>
2021-12-05 15:06:35 -0500

    ProcessString

---
+++ csharp/LeetCode/String/ProcessString.cs
@@ -0,0 +1,29 @@
+﻿namespace LeetCode.String.ProcessString
+{
+    using System.Collections.Generic;
+
+    public class Solution
+    {
+        public string ProcessString(string input)
+        {
+            var cur = input;
+            while (cur.Length > 3)
+            {
+                var num = 0;
+                var part = "";
+                for (var i = 0; i < cur.Length; i++)
+                {
+                    num += cur[i] - '0';
+                    if ((i + 1) % 3 == 0)
+                    {
+                        part += num;
+                        num = 0;
+                    }
+                }
+                part += num;
+                cur = part;
+            }
+            return cur;
+        }
+    }
+}
---
+++ csharp/UnitTest/String/ProcessStringTest.cs
@@ -0,0 +1,30 @@
+﻿namespace UnitTest.String
+{
+    using FluentAssertions;
+    using LeetCode.String.ProcessString;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class ProcessStringTest
+    {
+        private readonly Solution Solution = new Solution();
+
+        [TestMethod]
+        public void Test1()
+        {
+            Solution.ProcessString("11122223445").Should().Be("189");
+        }
+
+        [TestMethod]
+        public void Test2()
+        {
+            Solution.ProcessString("1").Should().Be("1");
+        }
+
+        [TestMethod]
+        public void Test3()
+        {
+            Solution.ProcessString("9999999").Should().Be("36");
+        }
+    }
+}
