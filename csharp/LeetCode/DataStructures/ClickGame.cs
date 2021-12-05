7b42b3baea26e4367ef324a3e771a19b407e1840
Shirui Peng <shirui.ryan.peng@gmail.com>
2021-12-05 17:36:12 -0500

    ClickGame

---
+++ csharp/LeetCode/DataStructures/ClickGame.cs
@@ -0,0 +1,75 @@
+﻿namespace LeetCode.DataStructures
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class ClickGame
+    {
+        int[][] b;
+        int m;
+        int n;
+        int[] dx = { -1, 0, 1, 0 };
+        int[] dy = { 0, -1, 0, 1 };
+        public ClickGame(int[][] board)
+        {
+            b = board;
+            m = board.Length;
+            n = board[0].Length;
+        }
+
+        public void Click(int x, int y)
+        {
+            if (x < 0 || x >= m || y < 0 || y >= n) return;
+            var empty = new (int, int)[3];
+            var mid = 1;
+            var t = b[x][y];
+            b[x][y] = 0;
+            empty[mid] = (x, y);
+            for (var d = 0; d < 4; d++)
+            {
+                var x1 = x + dx[d];
+                var y1 = y + dy[d];
+                if (x1 < 0 || x1 >= m || y1 < 0 || y1 >= n) continue;
+                if (b[x1][y1] == t)
+                {
+                    b[x1][y1] = 0;
+                    if (empty[mid + dy[d]].Item1 < x1)
+                    {
+                        empty[mid + dy[d]] = (x1, y1);
+                    }
+                }
+            }
+
+            for (var i = 0; i < 3; i++)
+            {
+                var (u, v) = empty[i];
+                if (b[u][v] == 0)
+                    Down(u, v);
+            }
+        }
+
+        public int[][] Get()
+        {
+            return b;
+        }
+
+        void Down(int row, int col)
+        {
+            var i = row;
+            while (i >= 0 && b[i][col] == 0) i--;
+            for (; i >= 0; i--)
+            {
+                Swap(i, col, row, col);
+                row--;
+            }
+        }
+
+        void Swap(int x, int y, int u, int v)
+        {
+            var t = b[x][y];
+            b[x][y] = b[u][v];
+            b[u][v] = t;
+        }
+    }
+}
---
+++ csharp/UnitTest/DataStructures/ClickGameTest.cs
@@ -0,0 +1,45 @@
+﻿namespace UnitTest.DataStructures
+{
+    using FluentAssertions;
+    using LeetCode.DataStructures;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class ClickGameTest
+    {
+        [TestMethod]
+        public void Test1()
+        {
+            int[][] b =
+            {
+                new int[] { 1, 2, 2 },
+                new int[] { 1, 2, 2 },
+                new int[] { 1, 2, 4 },
+                new int[] { 1, 2, 2 },
+
+            };
+            var t = new ClickGame(b);
+            t.Click(2, 1);
+            b[0].Should().Equal(1, 0, 2);
+            b[1].Should().Equal(1, 0, 2);
+            b[2].Should().Equal(1, 0, 4);
+            b[3].Should().Equal(1, 2, 2);
+            t.Click(0, 0);
+            b[0].Should().Equal(0, 0, 2);
+            b[1].Should().Equal(0, 0, 2);
+            b[2].Should().Equal(1, 0, 4);
+            b[3].Should().Equal(1, 2, 2);
+            t.Click(2, 2);
+            b[0].Should().Equal(0, 0, 0);
+            b[1].Should().Equal(0, 0, 2);
+            b[2].Should().Equal(1, 0, 2);
+            b[3].Should().Equal(1, 2, 2);
+            t.Click(3, 2);
+            b[0].Should().Equal(0, 0, 0);
+            b[1].Should().Equal(0, 0, 0);
+            b[2].Should().Equal(1, 0, 0);
+            b[3].Should().Equal(1, 0, 2);
+        }
+
+    }
+}
